6262e7acd022c74d95033c82db5e84d998066925
arcstur <arcstur@gmail.com>
2022-12-12 14:49:04 -0300

    day 04 part 2 solution

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+#[derive(Debug)]
 struct Assignment {
     start: u64,
     end: u64,
@@ -18,6 +19,10 @@ impl Assignment {
 
     fn contains(&self, other: &Self) -> bool {
         self.start <= other.start && self.end >= other.end
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        !((self.start > other.end) || (other.start > self.end))
     }
 }
 
@@ -54,12 +59,30 @@ impl AssignmentPair {
         }
         count
     }
+
+    fn has_overlapped(&self) -> bool {
+        self.0.overlaps(&self.1) || self.1.overlaps(&self.0)
+    }
+
+    fn count_overlapped(pairs: &Vec<AssignmentPair>) -> u64 {
+        let mut count = 0;
+        for pair in pairs {
+            if pair.has_overlapped() {
+                count += 1;
+            }
+        }
+        count
+    }
 }
 
 fn main() {
     let pairs = AssignmentPair::from_file("data/input.txt");
     let count_contained = AssignmentPair::count_contained(&pairs);
-    println!("Part 1: the number of pairst that have one assignment that contain the other is {}", count_contained);
+    println!("Part 1: the number of pairst that have one assignment that contains the other is {}", count_contained);
+
+    let pairs = AssignmentPair::from_file("data/input.txt");
+    let count_overlapped = AssignmentPair::count_overlapped(&pairs);
+    println!("Part 2: the number of pairst that have one assignment that overlaps the other is {}", count_overlapped);
 }
 
 #[cfg(test)]
@@ -75,6 +98,15 @@ mod tests {
         assert!(!Assignment{start: 7, end: 8}.contains(&Assignment{start: 6, end: 8}));
     }
 
+    #[test]
+    fn overlaps() {
+        assert!(Assignment{start: 0, end: 0}.overlaps(&Assignment{start: 0, end: 0}));
+        assert!(Assignment{start: 3, end: 5}.overlaps(&Assignment{start: 3, end: 4}));
+        assert!(Assignment{start: 5, end: 10}.overlaps(&Assignment{start: 6, end: 9}));
+        assert!(Assignment{start: 5, end: 6}.overlaps(&Assignment{start: 4, end: 6}));
+        assert!(Assignment{start: 7, end: 8}.overlaps(&Assignment{start: 6, end: 8}));
+    }
+
     fn test_path() -> &'static str {
         "data/test-input.txt"
     }
@@ -84,5 +116,12 @@ mod tests {
         let pairs = AssignmentPair::from_file(test_path());
         let count_contained = AssignmentPair::count_contained(&pairs);
         assert_eq!(count_contained, 2);
+    }
+
+    #[test]
+    fn part2() {
+        let pairs = AssignmentPair::from_file(test_path());
+        let count_overlapped = AssignmentPair::count_overlapped(&pairs);
+        assert_eq!(count_overlapped, 4);
     }
 }
