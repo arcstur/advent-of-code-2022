eeb15a97ea7b9cd871266010dcf6b4f385bc551a
arcstur <arcstur@gmail.com>
2022-12-12 11:38:24 -0300

    day 03 part 1 refactor, with tests

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,8 +1,6 @@
-#![allow(unused)]
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-
-mod rucksack {
+mod rsack {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
 
     static LETTERS: [char; 52] = [
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
@@ -12,20 +10,29 @@ mod rucksack {
 
     #[derive(Debug)]
     pub struct Rucksack {
-        pub left: Vec<char>,
-        pub right: Vec<char>,
+        items: Vec<char>,
     }
 
     impl Rucksack {
-        pub fn from(left: Vec<char>, right: Vec<char>) -> Rucksack {
-            Rucksack { left, right }
+        pub fn from(items: Vec<char>) -> Rucksack {
+            Rucksack { items }
+        }
+
+        pub fn left(&self) -> &[char] {
+            let middle = self.items.len() / 2;
+            &self.items[0..middle]
+        }
+
+        pub fn right(&self) -> &[char] {
+            let middle = self.items.len() / 2;
+            &self.items[middle..]
         }
 
         fn shared_chars(&self) -> Option<Vec<char>> {
             let mut shared: Vec<char> = Vec::new();
 
-            for character in &self.left {
-                if self.right.contains(character) && !shared.contains(character) {
+            for character in self.left() {
+                if self.right().contains(character) && !shared.contains(character) {
                     shared.push(character.to_owned());
                 }
             }
@@ -53,36 +60,102 @@ mod rucksack {
             }
             Some(priority)
         }
+
+        pub fn from_file(path: &str) -> Vec<Rucksack> {
+            let file = File::open(path).expect("Input file should exist");
+            let reader = BufReader::new(file);
+
+            let mut rucksacks: Vec<Rucksack> = Vec::new();
+
+            for line in reader.lines() {
+                let line = line.expect("Lines should be readable");
+                rucksacks.push(Rucksack::from(line.chars().collect()));
+            }
+
+            rucksacks
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn char_priorities() {
+            assert_eq!(
+                Rucksack::char_priority(&'a').expect("'a' char has a priority"),
+                1
+            );
+            assert_eq!(
+                Rucksack::char_priority(&'p').expect("'p' char has a priority"),
+                16
+            );
+            assert_eq!(
+                Rucksack::char_priority(&'L').expect("'L' char has a priority"),
+                38
+            );
+            assert_eq!(
+                Rucksack::char_priority(&'P').expect("'P' char has a priority"),
+                42
+            );
+            assert_eq!(
+                Rucksack::char_priority(&'t').expect("'t' char has a priority"),
+                20
+            );
+            assert_eq!(
+                Rucksack::char_priority(&'v').expect("'v' char has a priority"),
+                22
+            );
+            assert_eq!(
+                Rucksack::char_priority(&'s').expect("'s' char has a priority"),
+                19
+            );
+        }
+
+        #[test]
+        #[should_panic]
+        fn char_without_priority() {
+            Rucksack::char_priority(&'*').unwrap();
+        }
     }
 }
 
-use rucksack::Rucksack;
+use rsack::Rucksack;
 
 fn main() {
-    let input_path = "input.txt";
-    let file = File::open(input_path).expect("Input file should exist at input.txt");
-    let reader = BufReader::new(file);
-
-    let mut rucksacks: Vec<Rucksack> = Vec::new();
-
-    for line in reader.lines() {
-        let line = line.expect("Lines should be readable");
-
-        let mut left: Vec<char> = line.chars().collect();
-        if left.len() % 2 != 0 {
-            panic!("Every line should contain an even number of characters");
-        }
-        let right = left.split_off(left.len() / 2);
-
-        rucksacks.push(Rucksack::from(left, right));
-    }
+    let rucksacks = Rucksack::from_file("input.txt");
 
     let mut priorities = 0;
-    for rucksack in rucksacks {
+    for rucksack in &rucksacks {
         priorities += rucksack
             .priority()
             .expect("Every rucksack in the input file should have a priority");
     }
 
-    println!("The sum of the rucksacks' priorities is {}", priorities);
+    println!(
+        "Part 1: The sum of the rucksacks' priorities is {}",
+        priorities
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_path() -> &'static str {
+        "test-input.txt"
+    }
+
+    #[test]
+    fn part1() {
+        let rucksacks = Rucksack::from_file(test_path());
+        let mut priorities = 0;
+        for rucksack in &rucksacks {
+            priorities += rucksack
+                .priority()
+                .expect("Every rucksack in the input file should have a priority");
+        }
+
+        assert_eq!(priorities, 157);
+    }
 }
